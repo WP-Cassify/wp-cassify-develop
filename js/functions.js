49e82fa9a0aa95d2efd174453613dc467a359bf9
aaf017 <aaf017@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2016-03-28 14:13:31 +0000

    Define notifications rules based on user attributes values
    git-svn-id: https://plugins.svn.wordpress.org/wp-cassify/tags/1.6.2@1380367 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,36 @@
-
 jQuery( document ).ready(function() {
     jQuery( '#wp_cassify_custom_user_meta' ).hide();
+
+	// for metaboxes
+	jQuery(".if-js-closed").removeClass("if-js-closed").addClass("closed");
+	postboxes.add_postbox_toggles( 'wp-cassify');
+
+	// scroll to last submit button clicked.
+	if( typeof wp_cassify_screen_data.scrollToId != 'undefined') {
+
+		var wp_cassify_id_to_scroll = '';
+		
+		if ( wp_cassify_screen_data.scrollToId == 'wp_cassify_send_notification_test_message' ) {
+			wp_cassify_id_to_scroll = '#wp_cassify_metabox_notifications_settings';
+		}
+		else {
+			wp_cassify_id_to_scroll = '#' +  wp_cassify_screen_data.scrollToId.replace( 'save_options', 'metabox' );
+		}
+
+		jQuery( 'html, body' ).animate({
+        		scrollTop: jQuery( wp_cassify_id_to_scroll ).offset().top
+    		}, 
+    		500
+		);
+	}
+	
+	// Disabled field if checkbox unchecked
+    if( jQuery( '#wp_cassify_notifications_smtp_auth' ).is(':checked') ) {
+    	jQuery( '#wp_cassify_notifications_encryption_type' ).prop( 'disabled', false );
+    }
+    else {
+    	jQuery( '#wp_cassify_notifications_encryption_type' ).prop( 'disabled', 'disabled' );
+    }	
 });
 
 jQuery( '#wp_cassify_add_rule' ).click( function ( evt ) {
@@ -137,9 +167,75 @@ jQuery( '#wp_cassify_wordpress_user_meta_list' ).change( function ( evt ) {
 	return false;
 });
 
-jQuery( '#wp_cassify_save_options' ).click( function ( evt ) {
+jQuery( '#wp_cassify_notifications_smtp_auth' ).change(function() {
+    
+    console.log('ok');
+    
+    if( this.checked ) {
+        jQuery( '#wp_cassify_notifications_encryption_type' ).prop('disabled', false);
+    }
+    else {
+    	jQuery( '#wp_cassify_notifications_encryption_type' ).prop('disabled', 'disabled');
+    }
+    
+	evt.preventDefault();
+	return false;
+});
+
+
+jQuery( '#wp_cassify_add_notification_rule' ).click( function ( evt ) {
+
+	var wp_cassify_notification_action = jQuery( '#wp_cassify_notifications_actions option:selected' ).val();
+	var wp_cassify_notification_rule = jQuery( '#wp_cassify_notification_rule' ).val();
+	
+	jQuery( '#wp_cassify_notification_rules' )
+         .append(
+         	jQuery( '<option></option>' )
+         		.attr( 'value', wp_cassify_notification_action + '|' + wp_cassify_notification_rule ) 
+         		.text( wp_cassify_notification_action + '|' + wp_cassify_notification_rule )
+     		); 	
+
+	evt.preventDefault();
+	return false;
+});
+
+jQuery( '#wp_cassify_remove_notification_rule' ).click( function ( evt ) {
+	
+	jQuery( '#wp_cassify_notification_rules option:selected' ).remove();
+	
+	evt.preventDefault();	
+	return false;
+});
+
+jQuery( '#wp_cassify_notification_rules' ).dblclick( function ( evt ) {
+
+	jQuery( '#wp_cassify_notification_rule' ).val( jQuery( '#wp_cassify_notification_rules option:selected' ).val().split( '|' )[ 1 ] );
+	jQuery( '#wp_cassify_default_notifications' ).val( jQuery( '#wp_cassify_notification_rules option:selected' ).val().split( '|' )[ 0 ] );
+	jQuery( '#wp_cassify_notification_rules option:selected' ).remove();
+
+	evt.preventDefault();	
+	return false;
+});
+
+jQuery( '[data-style="wp_cassify_save_options"]' ).click( function ( evt ) {
 	
 	jQuery( '#wp_cassify_autorization_rules option' ).prop( 'selected', true );
 	jQuery( '#wp_cassify_user_role_rules option' ).prop( 'selected', true );
 	jQuery( '#wp_cassify_user_attributes_mapping_list option' ).prop( 'selected', true );
+	jQuery( '#wp_cassify_notification_rules option' ).prop( 'selected', true );
+	
+	if ( jQuery( '#wp_cassify_notifications_smtp_password' ).val() != jQuery( '#wp_cassify_notifications_smtp_confirm_password' ).val() ) {
+		alert( 'SMPT Password does not macth confirmation !');
+		
+		evt.preventDefault();
+		return false;
+	}
+
+	if ( jQuery.inArray( jQuery( '#wp_cassify_notifications_salt' ).val().length, [ 16, 24, 32 ] ) ) {
+		alert( 'Salt error : only keys of sizes 16, 24 or 32 supported');
+		
+		evt.preventDefault();
+		return false;
+	}	
+	
 });
