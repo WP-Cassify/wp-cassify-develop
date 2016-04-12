7da6ff47fafa2af2df59602755591c67622faecc
aaf017 <aaf017@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2016-04-12 10:27:15 +0000

    Suscriber role is pushed by default when user is successfully authenticated by CAS.
    git-svn-id: https://plugins.svn.wordpress.org/wp-cassify/tags/1.6.7@1392831 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- classes/wp_cassify_utils.php
+++ classes/wp_cassify_utils.php
@@ -8,12 +8,19 @@ class WP_Cassify_Utils {
 	 * cas-user id and cas-user attributes.
 	 * @param string $url
 	 * @param string $ssl_cipher
+	 * @param string $ssl_check_certificate
 	 * @return string $response
 	 */ 
-	public static function wp_cassify_do_ssl_web_request( $url, $ssl_cipher ) {
+	public static function wp_cassify_do_ssl_web_request( $url, $ssl_cipher, $ssl_check_certificate = 'disabled' ) {
 		
 		if (! function_exists ( 'curl_init' ) ) {
 			die( 'Please install php cURL library !');
+		}
+
+		$curlopt_ssl_verify_peer = 0;
+		
+		if ( $ssl_check_certificate == 'enabled' ) {
+			$curlopt_ssl_verify_peer = 1;
 		}
 
 		$ch = curl_init();
@@ -21,8 +28,8 @@ class WP_Cassify_Utils {
 		curl_setopt( $ch, CURLOPT_HEADER, false );
 		curl_setopt( $ch, CURLOPT_URL, $url ) ;
 		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
-		curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, 1);
-		curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, 1);
+		curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, 2);
+		curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, $curlopt_ssl_verify_peer );
 		
 		//curl_setopt( $ch, CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13' );
 		curl_setopt( $ch, CURLOPT_SSLVERSION, $ssl_cipher );
@@ -57,8 +64,15 @@ class WP_Cassify_Utils {
 	public static function wp_cassify_get_current_url( $wp_cassify_default_wordpress_blog_http_port, $wp_cassify_default_wordpress_blog_https_port ) {
 		
 		$current_url = ( @$_SERVER[ 'HTTPS' ] == 'on' ) ? 'https://' : 'http://';
-		$current_url .= $_SERVER[ 'SERVER_NAME' ];
-	 
+		
+		// If cassified application is hosted behind reverse proxy.
+		if ( isset( $_SERVER[ 'HTTP_X_FORWARDED_HOST' ] ) ) {
+			$current_url .= $_SERVER[ 'HTTP_X_FORWARDED_HOST' ];
+		}
+		else {
+			$current_url .= $_SERVER[ 'SERVER_NAME' ];
+		}
+		
 		if( ( $_SERVER[ 'SERVER_PORT' ] != $wp_cassify_default_wordpress_blog_http_port ) && 
 			( $_SERVER[ 'SERVER_PORT' ] != $wp_cassify_default_wordpress_blog_https_port ) ) {
 			$current_url .= ':' . $_SERVER[ 'SERVER_PORT' ];
@@ -154,6 +168,22 @@ class WP_Cassify_Utils {
 	}
 	
 	/**
+	 * Check if wordpress user account already exist
+	 * @param string $cas_user_id
+	 * @return bool $is_wordpress_user_exist
+	 */ 
+	public static function wp_cassify_is_wordpress_user_exist( $cas_user_id ) {
+
+		$is_wordpress_user_exist = TRUE;
+
+		if (! username_exists( $cas_user_id ) ) {
+			$is_wordpress_user_exist = FALSE;
+		}
+		
+		return $is_wordpress_user_exist;
+	}	
+	
+	/**
 	 * Set role to an existing Wordpress user
 	 * @param string $wordpress_user_login
 	 * @param string $role_key
@@ -219,6 +249,225 @@ class WP_Cassify_Utils {
         
         return $wp_cassify_plugin_option;
     }
+    
+    /**
+     * Set plugin option according to activation plugin level
+     * @param bool $network_activated
+     * @param string $option_name
+     * @param string $option_value
+     */
+    public static function wp_cassify_update_option( $network_activated, $option_name, $option_value ) {
+        
+        $wp_cassify_plugin_option = '';
+        
+        if ( $network_activated ) {
+            update_site_option( $option_name , sanitize_text_field( $option_value ) );
+        }
+        else {
+            update_option( $option_name , sanitize_text_field( $option_value ) );
+        }
+    }    
+
+    /**
+     * Save plugin options stored in form textfield into database.
+     * @param array $post_array
+     * @param string $field_name
+     * @param bool $do_not_check_empty
+     * @param bool $wp_cassify_network_activated
+     */
+    public static function wp_cassify_update_textfield( &$post_array, $field_name, $do_not_check_empty = FALSE, $wp_cassify_network_activated ) {
+
+		$field_value = '';
+		
+		if (! $do_not_check_empty ) {
+			if(! empty( $post_array[ $field_name ] ) ) {
+	        	$field_value = $post_array[ $field_name ];
+	        }
+		}
+		else {
+			$field_value = $post_array[ $field_name ];
+		}
+		
+        if ( $wp_cassify_network_activated ) {
+            update_site_option( $field_name , sanitize_text_field( $field_value ) );
+        }
+        else {
+            update_option( $field_name , sanitize_text_field( $field_value ) );
+        }                
+    }
+    
+    /**
+     * Save plugin options stored in form textfield into database.
+     * @param string $field_value
+     * @param string $field_name
+     * @param bool $wp_cassify_network_activated
+     */
+    public static function wp_cassify_update_textfield_manual( $field_value, $field_name, $wp_cassify_network_activated ) {
+
+        if( isset( $field_value ) ) {
+            if ( $wp_cassify_network_activated ) {
+                update_site_option( $field_name , sanitize_text_field( $field_value ) );
+            }
+            else {
+                update_option( $field_name , sanitize_text_field( $field_value ) );
+            }                
+        }
+    }    
+        
+    /**
+     * Save plugin options stored in form checkbox into database.
+     * @param array $post_array
+     * @param string $field_name
+     * @param bool $wp_cassify_network_activated
+     */
+    public static function wp_cassify_update_checkbox( &$post_array, $field_name, $checked_value_name, $wp_cassify_network_activated ) {
+
+        if ( (! empty( $post_array[ $field_name ] ) ) && ( $post_array[ $field_name ] == $checked_value_name ) ) {
+            if ( $wp_cassify_network_activated ) {
+                update_site_option( $field_name , $post_array[ $field_name ] );
+            }
+            else {
+                update_option( $field_name , $post_array[ $field_name ] );
+            }      
+        }
+        else {
+            if ( $wp_cassify_network_activated ) {
+                update_site_option( $field_name , '' );
+            }
+            else {
+                update_option( $field_name , '' );
+            }
+        }	            
+    } 
+    
+    /**
+     * Save plugin options stored in form multiple select into database.
+     * @param array $post_array
+     * @param string $field_name
+	 * @param bool $wp_cassify_network_activated
+     */
+    public static function wp_cassify_update_multiple_select( &$post_array, $field_name, $wp_cassify_network_activated  ) {
+        
+		$field_value = '';
+
+        if(! empty( $post_array[ $field_name ] ) ) {
+        	$field_value = $post_array[ $field_name ];
+        	
+            if( !is_serialized( $field_value ) ) {
+                $field_value = serialize( $field_value );
+            }
+        }
+        
+        if ( $wp_cassify_network_activated ) {
+            update_site_option( $field_name , sanitize_text_field( $field_value ) );
+        }
+        else {
+            update_option( $field_name , sanitize_text_field( $field_value ) );
+        }        
+    }      
+    
+	/**
+	 * Function used to encrypt data
+	 * @return $encrypted_string
+	 */ 
+	public static function wp_cassify_simple_encrypt( $text, $salt = "wp_cassify" ) {
+		
+		if (! function_exists ( 'mcrypt_encrypt' ) ) {
+			die( 'Please install php mcrypt library !');
+		}		
+		
+		$encrypted_string = trim( 
+			base64_encode( 
+				mcrypt_encrypt( 
+					MCRYPT_RIJNDAEL_256, 
+					$salt, 
+					$text, 
+					MCRYPT_MODE_ECB, 
+					mcrypt_create_iv( 
+						mcrypt_get_iv_size( MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB ), 
+						MCRYPT_RAND 
+					) 
+				)
+			)
+		);
+		
+		return $encrypted_string;
+	}
+
+	/**
+	 * Function used to decrypt data
+	 * @return $decrypted_string
+	 */ 
+	public static function wp_cassify_simple_decrypt( $text, $salt = "wp_cassify" ) {
+		
+		if (! function_exists ( 'mcrypt_decrypt' ) ) {
+			die( 'Please install php mcrypt library !');
+		}			
+		
+		$decrypted_string = mcrypt_decrypt(
+			MCRYPT_RIJNDAEL_256, 
+			$salt, 
+			base64_decode( $text ), 
+			MCRYPT_MODE_ECB, 
+			mcrypt_create_iv( 
+				mcrypt_get_iv_size( 
+					MCRYPT_RIJNDAEL_256, 
+					MCRYPT_MODE_ECB
+				), 
+				MCRYPT_RAND
+			)
+		);
+		
+		return $decrypted_string;
+	}    
+	
+	/**
+	 * Function used by plugin to send mail.
+	 * @param string $from
+	 * @param $string $to
+	 * @param $string $subject
+	 * @param $string $body
+	 * @param $string $smtp_host
+	 * @param $string $smtp_port
+	 * @param $string $smtp_auth
+	 * @param $string $smtp_password
+	 * @return $send_result
+	 */ 
+	public static function wp_cassify_sendmail( $from, $to, $subject, $body, $priority, $smtp_host, $smtp_port = 25, $smtp_auth = false, $smtp_encryption_type, $smtp_user, $smtp_password ) {
+		
+		// Create the Transport
+		$transport = \Swift_SmtpTransport::newInstance( $smtp_host, $smtp_port )
+			->setUsername( $smtp_user )
+			->setPassword( $smtp_password );
+			
+		if ( $smtp_auth == TRUE ) {
+			$transport->setEncryption( $smtp_encryption_type );
+			// $transport->setStreamOptions( array( $smtp_encryption_type => array( 'allow_self_signed' => true, 'verify_peer' => false ) ) );
+		}
+		
+		// Create the Mailer using your created Transport
+		$mailer = \Swift_Mailer::newInstance( $transport );			
+		
+		// Create the message
+		$message = \Swift_Message::newInstance()
+			->setSubject( $subject )
+			->setFrom( array( $from => $from ) )
+			->setTo( array( $to ) )
+			->setBody( $body )
+			->setContentType( 'text/html; charset=UTF-8')
+			->setReturnPath( $from )
+			->setPriority( $priority );
+			
+		// Send the message
+		if (! $mailer->send( $message, $failures ) ) {
+			$send_result = $failures;
+		}
+		else {
+			$send_result = TRUE;
+		}
+		
+		return $send_result;
+	}
 }
 
 ?>
--- readme.txt
+++ readme.txt
@@ -1,10 +1,11 @@
 === Plugin Name ===
+
 Contributors: aaf017
-Tags: Auth, authentication, CAS, central, centralized, integration, ldap, Cassify, phpCAS, server, service, system, JASIG, JASIG CAS
+Tags: Auth, authentication, CAS, central, centralized, integration, ldap, Cassify, phpCAS, server, service, system, JASIG, JASIG CAS, CAS Authentication, central authentication service, access, authorization, education
 Donate link: https://wpcassify.wordpress.com/donate/
 Requires at least: 4.4
-Tested up to: 4.4.2
-Stable tag: 1.5.1
+Tested up to: 4.5
+Stable tag: 1.6.7
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -23,22 +24,32 @@ https://wpcassify.wordpress.com/
 
 = Requirements =
 
-* Require php5-curl package
 * Require at least PHP version 5.3.10
+* Require at least PHP CURL package (for example, 'apt-get install php5-curl' if you're using php5 on debian systems).
 
 = Features included =
 
-* (New Feature !) Synchronize Wordpress User metas with CAS User attributes
-* (New Feature !) Set conditionnal users roles
+* Suscriber role is pushed by default when user is successfully authenticated by CAS.
+* Add support for web application hosted behind a reverse proxy. (Thanks to franck86)
+
+
 * Tested with CAS Server version 4.1.4
 * Compatible with CAS Protocol version 2 and 3
+* Synchronize Wordpress User metas with CAS User attributes
+* Set up Wordpress Roles to User according to CAS User attributes
 * Authorization rule editor
 * Automatic user creation if not exist in Wordpress database
 * Compatible with Wordpress Access Control Plugin
 * You can choose CAS User attributes you want to populate. Then you can access them via PHP Session
 * Manage URL White List to bypass CAS Authentication on certain pages
+* Receive email notifications when trigger is fired (after user account creation, after user login/logout).
+* Define notifications rules based on user attributes values.
 * Network activation allowed
-* You can set logout URL. This perform a logout redirection avec CAS Logout
+* You can set Service Logout URL (Needs to have CAS Server with followServiceRedirects option configured).
+* Custom hook to perform actions just after cas authentication. Hook name : wp_cassify_after_cas_authentication. (See online documentation, Screencast available)
+* Custom filter to perform custom cas server response parsing. Hook name : wp_cassify_custom_parsing_cas_xml_response (See online documentation)
+* Custom shortcode to generate CAS login link into your blog. (See online documentation)
+
 
 == Installation ==
 
@@ -53,10 +64,18 @@ e.g.
 == Screenshots ==
 
 1. This is the basic options of the plugin.
-2. This is the authorization rule editor.
-3. This is the user role rule editor. Set users roles according CAS User Attributes.
+2. This is the URL settings.
+3. This is the attributes extraction settings.
+4. This is the authorization rule editor.
+5. You can set wordpress role to user according to CAS attributes values.
+6. Synchronize Wordpress User metas with CAS User attributes.
+7. This is the email notifications settings.
+8. Define email notifications based on triggers and user attributes.
 
 == Frequently Asked Questions ==
+
+* Where can i find plugin documentation ?
+You can find documentation here (See Plugin options and Screencasts) : https://wpcassify.wordpress.com/
 
 * How to perform CAS Authentication on a front Office page ?
 Install WordPress Access Control Plugin. And mark page as "Only accessible by members". You can use anoter ACL plugin. If page require an authentication, CAS Authentication is fired.
@@ -69,16 +88,69 @@ Contact me at aa_francois@yahoo.fr and i try answer to your question.
 
 == Changelog ==
 
-= 1.0 =
-* First version.
+= 1.6.7 =
+* Suscriber role is pushed by default when user is successfully authenticated by CAS.
 
-= 1.2 =
-* Multisite configuration possible if network activated.
+= 1.6.6 =
+* Bug fix
+* Add custom hook : wp_cassify_before_auth_user_wordpress
+
+= 1.6.5 =
+* Add support for web application hosted behind a reverse proxy. (Thanks to franck86).
+
+= 1.6.4 =
+* Add shortcode to generate login CAS link with redirect.
+
+= 1.6.3 =
+* Fix bug on javascript control UI.
+
+= 1.6.2 =
+* Send email notifications when trigger is fired (after user account creation, after user login/logout).
+
+= 1.6.1 =
+* Email notifications when user account is created.
+
+= 1.6.0 =
+* New admin interface with metaboxes.
+* Fix Bug multi-select fields if network activated.
+
+= 1.5.9 =
+* Fix Bug on synchronization between CAS User attributes and Wordpress User metas
+
+= 1.5.8 =
+* Fix SSL Certificate probleme. Reintroduce cURL function instead of wp_remote_get function.
+* Add SSL Configuration option : SSL Check Certificate wich turn on/off CURLOPT_SSL_VERIFYPEER option.
+
+= 1.5.7 =
+* Replace cURL function with wp_remote_get function. Now plugin does not require php5-curl.
+* Add notice message on admin screen.
+
+= 1.5.6 =
+* (New Feature !) Custom hook to perform actions just after cas authentication. Hook name : wp_cassify_after_cas_authentication
+* (New Feature !) Custom hook to perform custom cas server response parsing. Hook name : wp_cassify_custom_parsing_cas_xml_response
+
+= 1.5.5 =
+* Some bug fixes.
+
+= 1.5.4 =
+* Add custom GET parameter (?wp_cassify_bypass=bypass) to bypass CAS Authentication on certain urls. See online documentation for more infos. Bug fixes on Authorization rule editor.
+
+= 1.5.3 =
+* Some bug fixes. Security fixes.
+
+= 1.5.2 =
+* Some bug fixes.
+
+= 1.5.1 =
+* Synchronize Wordpress User metas with CAS User attributes
 
 = 1.5 =
 * User Role Rule Editor : set user wordpress role according to CAS Attributes values.
 
-= 1.5.1 =
-* Synchronize Wordpress User metas with CAS User attributes
+= 1.2 =
+* Multisite configuration possible if network activated.
+
+= 1.0 =
+* First version.
 
 == Upgrade Notice ==
