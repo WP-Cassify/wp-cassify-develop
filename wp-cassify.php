ea20aaa06ac96b6e2dab387baecc0f1b48b94ef9
aaf017 <aaf017@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2016-04-29 07:37:51 +0000

    Bug fix on subscriber pushing role
    git-svn-id: https://plugins.svn.wordpress.org/wp-cassify/tags/1.7.8@1407135 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-cassify.php
+++ wp-cassify.php
@@ -4,7 +4,7 @@
  * Plugin Name: WP Cassify
  * Plugin URI: https://wpcassify.wordpress.com/
  * Description: CAS Authentication Client for Wordpress. Also, it handle custom authorizations rules from cas user attributes.
- * Version: 1.5.1
+ * Version: 1.7.8
  * Author: Alain-Aymerick FRANCOIS
  * Author URI: https://wpcassify.wordpress.com/a-propos/
  * License: GPLv2
@@ -12,7 +12,7 @@
  
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
-if (! function_exists( 'get_plugin_data' )) {
+if (! function_exists( 'get_plugin_data' ) ) {
 	require_once ABSPATH . '/wp-admin/includes/plugin.php';
 }
 
@@ -21,9 +21,13 @@ $wp_cassify_plugin_directory = plugin_dir_url( __FILE__ );
 $wp_cassify_network_activated = FALSE;
 
 include( plugin_dir_path( __FILE__ ) . 'config.php');
+
+include( plugin_dir_path( __FILE__ ) . 'classes/vendor/swiftmailer/swiftmailer/lib/swift_required.php');
 include( plugin_dir_path( __FILE__ ) . 'classes/wp_cassify_utils.php');
 include( plugin_dir_path( __FILE__ ) . 'classes/wp_cassify_rule_solver.php');
 include( plugin_dir_path( __FILE__ ) . 'classes/wp_cassify_plugin.php');
+include( plugin_dir_path( __FILE__ ) . 'classes/wp_cassify_shortcodes.php');
+
 include( plugin_dir_path( __FILE__ ) . 'admin/admin-menu.php');
 
 /**
@@ -34,15 +38,15 @@ register_deactivation_hook( __FILE__, 'wp_cassify_deactivation' );
 function wp_cassify_deactivation() {
 	
 	global $wp_cassify_plugin_options_list;
-        global $wp_cassify_network_activated;
+    global $wp_cassify_network_activated;
 	
 	foreach ( $wp_cassify_plugin_options_list as $option ) {
-            if ( $wp_cassify_network_activated ) {
-                delete_site_option( $option );
-            }
-            else {
-                delete_option( $option );
-            }
+        if ( $wp_cassify_network_activated ) {
+            delete_site_option( $option );
+        }
+        else {
+            delete_option( $option );
+        }
 	}  
 }
 
@@ -69,8 +73,11 @@ $wp_cassify_admin_page->init_parameters(
 		$wp_cassify_default_logout_servlet,
 		$wp_cassify_default_service_validate_servlet,
 		$wp_cassify_default_ssl_cipher_values,
+		$wp_cassify_default_ssl_check_certificate,
 		$wp_cassify_default_xpath_query_to_extact_cas_user,
 		$wp_cassify_default_xpath_query_to_extact_cas_attributes,
+		$wp_cassify_default_notifications_options,
+		$wp_cassify_default_expirations_options,
 		$wp_cassify_default_allow_deny_order,
 		$wp_cassify_wordpress_user_meta_list
 );
@@ -83,8 +90,10 @@ $GLOBALS['wp-cassify']->init_parameters(
 		$wp_cassify_default_redirect_parameter_name,
 		$wp_cassify_default_service_ticket_parameter_name,
 		$wp_cassify_default_service_service_parameter_name,
+		$wp_cassify_default_bypass_parameter_name,
 		$wp_cassify_default_wordpress_blog_http_port,
 		$wp_cassify_default_wordpress_blog_https_port,
+		$wp_cassify_default_ssl_check_certificate,
 		$wp_cassify_default_login_servlet,
 		$wp_cassify_default_logout_servlet,
 		$wp_cassify_default_service_validate_servlet,
@@ -95,6 +104,20 @@ $GLOBALS['wp-cassify']->init_parameters(
 		$wp_cassify_allowed_operators,
 		$wp_cassify_operator_prefix,
 		$wp_cassify_allowed_parenthesis,
-		$wp_cassify_error_messages
+		$wp_cassify_allowed_get_parameters,
+		$wp_cassify_error_messages,
+		$wp_cassify_user_error_codes
 );
+
+$wp_cassify_shortcodes = new \wp_cassify\WP_Cassify_Shortcodes();
+$wp_cassify_shortcodes->init_parameters(
+	$wp_cassify_network_activated,
+	$wp_cassify_default_redirect_parameter_name,
+	$wp_cassify_default_service_service_parameter_name,
+	$wp_cassify_default_wordpress_blog_http_port,
+	$wp_cassify_default_wordpress_blog_https_port,
+	$wp_cassify_default_login_servlet,
+	$wp_cassify_default_logout_servlet
+);
+
 ?>
