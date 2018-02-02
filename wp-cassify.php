c24b513a9f69a222bc311faef341d47ae5896ce4
aaf0171 <aa_francois@yahoo.fr>
2018-02-02 11:01:19 +0000

    Add new filter wp_cassify_override_service_validate_url
    git-svn-id: https://plugins.svn.wordpress.org/wp-cassify/trunk@1814035 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-cassify.php
+++ wp-cassify.php
@@ -4,7 +4,7 @@
  * Plugin Name: WP Cassify
  * Plugin URI: https://wpcassify.wordpress.com/
  * Description: CAS Authentication Client for Wordpress. Also, it handle custom authorizations rules from cas user attributes.
- * Version: 2.0.6
+ * Version: 2.0.7
  * Author: Alain-Aymerick FRANCOIS
  * Author URI: https://wpcassify.wordpress.com/about-me/
  * License: GPLv2
