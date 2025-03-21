38b569f073bd97bdc66b6e2b74f846f5e4df7994
Vincent Bonamy <bonamvin@debian-BULLSEYE-live-builder-AMD64>
2025-03-21 09:31:21 +0100

    Readme and plugin header for the next release (2.3.7) ready

--- wp-cassify/wp-cassify.php
+++ wp-cassify/wp-cassify.php
@@ -4,7 +4,7 @@
  * Plugin Name: WP Cassify
  * Plugin URI: https://wpcassify.wordpress.com/
  * Description: CAS Authentication Client for Wordpress. Also, it handle custom authorizations rules from cas user attributes.
- * Version: 2.3.6
+ * Version: 2.3.7
  * Requires PHP: 7.0
  * Author: Alain-Aymerick FRANCOIS
  * Author URI: https://wpcassify.wordpress.com/about-me/
