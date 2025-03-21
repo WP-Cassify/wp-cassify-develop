ce658a9cebf61daaff5a3babe490ffc4647e4c64
Vincent Bonamy <bonamvin@debian-BULLSEYE-live-builder-AMD64>
2025-03-21 09:27:54 +0100

    Add GitHub Actions workflow for testing & fix rule evaluation

--- wp-cassify/test.php
+++ wp-cassify/test.php
@@ -1,39 +1,59 @@
 <?php
 
-// Test the plugin with php-cli : 
-// - sudo chmod +x test.php
-// - php ./test.php
-
-require_once __DIR__ . '/config.php';
 require_once __DIR__ . '/classes/wp_cassify_rule_solver.php';
 
-$mock_cas_object = array(
-	'first_name' => 'Maria2',
-	'email' => 'awoods1a@toplist.cz',
-	'eduPersonAffiliation' => array('alumn','student','affiliate')
-);
+function run_tests() {
 
-// - One operator per parenthesis group like this : (...-AND...)
-// - Two level parenthesis maximum are allowed. The first with square brackets and the sub-level with brackets like this : [(...-OR...) -AND (...-AND...)]
-// - 
-// $condition = '[(CAS{cas_user_id} -EQ "tferguson4") -AND (CAS{courriel} -CONTAINS "my-university.fr")] -OR (CAS{cas_user_id} -STARTWITH "test") -OR [(CAS{cas_user_id} -EQ "tferguson4") -OR (CAS{courriel} -STARTWITH "my-university.fr")]';
-//$condition = '(CAS{first_name} -EQ "Maria") -AND (CAS{email} -CONTAINS "mhawkins0@mashable.com")';
-// $condition = '(CAS{cas_user_id} -EQ "tferguson4") -AND (CAS{courriel} -CONTAINS "my-university.fr")';
-// $condition = '(CAS{cas_user_id} -EQ "tferguson4") -AND (CAS{courriel} -CONTAINS "my-university.fr") -OR (CAS{cas_user_id} -STARTWITH "test")';
-$condition = '(CAS{eduPersonAffiliation} -IN "alumn")';
+    require_once __DIR__ . '/config.php';
+    
+    $mock_cas_object = [
+        'first_name' => 'Maria2',
+        'email' => 'awoods1a@toplist.cz',
+        'eduPersonAffiliation' => ['alumn', 'student', 'affiliate']
+    ];
 
+    $solver = new \wp_cassify\wp_cassify_rule_solver();
 
-$solver = new \wp_cassify\wp_cassify_rule_solver();
+    $solver->match_first_level_parenthesis_group_pattern = $wp_cassify_match_first_level_parenthesis_group_pattern;
+    $solver->match_second_level_parenthesis_group_pattern = $wp_cassify_match_second_level_parenthesis_group_pattern;
+    $solver->match_cas_variable_pattern = $wp_cassify_match_cas_variable_pattern;
+    $solver->allowed_operators = $wp_cassify_allowed_operators;
+    $solver->operator_prefix = $wp_cassify_operator_prefix;
+    $solver->allowed_parenthesis = $wp_cassify_allowed_parenthesis;
+    $solver->error_messages = $wp_cassify_error_messages;
+    $solver->cas_user_datas = $mock_cas_object;
 
-$solver->match_first_level_parenthesis_group_pattern = $wp_cassify_match_first_level_parenthesis_group_pattern;
-$solver->match_second_level_parenthesis_group_pattern = $wp_cassify_match_second_level_parenthesis_group_pattern;
-$solver->match_cas_variable_pattern = $wp_cassify_match_cas_variable_pattern;
-$solver->allowed_operators = $wp_cassify_allowed_operators;
-$solver->operator_prefix = $wp_cassify_operator_prefix;
-$solver->allowed_parenthesis = $wp_cassify_allowed_parenthesis;
-$solver->error_messages = $wp_cassify_error_messages;
-$solver->cas_user_datas = $mock_cas_object;
+    $tests = [
+        '(CAS{first_name} -EQ "Maria") -AND (CAS{email} -CONTAINS "mhawkins0@mashable.com")' => false,
+        '(CAS{first_name} -EQ "Maria2") -AND (CAS{email} -CONTAINS "awoods1a@toplist.cz")' => true,
+        '[(CAS{eduPersonAffiliation} -CONTAINS "student") -AND (CAS{email} -ENDWITH "toplist.cz")]' => true,
+        '(CAS{eduPersonAffiliation} -IN "alumn")' => true,
+        '(CAS{eduPersonAffiliation} -IN "al")' => false,
+        '(CAS{eduPersonAffiliation} -NOTIN "alumn")' => false,
+        '(CAS{eduPersonAffiliation} -NOTIN "student")' => false,        
+        '(CAS{eduPersonAffiliation} -NOTIN "al")' => true,
+        '(CAS{eduPersonAffiliation} -CONTAINS "al")' => true,
+        '(CAS{eduPersonAffiliation} -NCONTAINS "la")' => true,
+        '(CAS{eduPersonAffiliation} -NCONTAINS "al")' => false,
+    ];
 
-var_dump( $solver->solve( $condition ) );
+    $all_tests_passed = true;
+    foreach ($tests as $condition => $expected) {
+        $result = $solver->solve($condition);
+        assert($result === $expected, "Test failed: '$condition' expected " . var_export($expected, true) . " but got " . var_export($result, true));
+        if($result === $expected) {
+            echo "\tTest OK : '$condition' => " . var_export($expected, true) . "\n";
+        } else {
+            $all_tests_passed = false;
+            echo "\tTest KO : '$condition' => " . var_export($result, true) . " but " . var_export($expected, true) . " was expected\n";
+        }
+    }
 
-?>
+    if (!$all_tests_passed) {
+        exit(1);
+    }
+}
+
+run_tests();
+
+echo "All tests executed.\n";
